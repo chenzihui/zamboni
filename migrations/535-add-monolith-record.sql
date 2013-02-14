d236ddd4cba76c24fe35f9895edd0de5022496fd
Alexis Métaireau <alexis@notmyidea.org>
2013-02-14 16:11:47 +0100

    we want datetime, not date.

--- migrations/535-add-monolith-record.sql
+++ migrations/535-add-monolith-record.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `monolith_record` (
     `id` int(11) unsigned AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `key` varchar(255) NOT NULL,
-    `recorded` date NOT NULL,
+    `recorded` datetime NOT NULL,
     `user` varchar(255) NOT NULL,
     `value` longtext NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
